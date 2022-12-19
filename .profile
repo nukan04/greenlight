c83e406e24e7c9da95e5133ef178cd9bc7a515e1
User <nukan04@mail.ru>
2022-12-20 02:31:42 +0600

    connect to PostgreSQL with driver pgx

---
+++ .profile
@@ -0,0 +1,2 @@
+# I THINK THIS FILE SHOULD TO CREATE IN PROGRAM FILES
+export GREENLIGHT_DB_DSN='postgres://greenlight:123456@localhost/greenlight'
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
+	_ "github.com/jackc/pgx/v5/stdlib"
 	"log"
 	"net/http"
 	"os"
@@ -14,6 +17,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 type application struct {
 	config config
@@ -22,10 +28,21 @@ type application struct {
 
 func main() {
 	var cfg config
+
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://greenlight:123456@localhost/greenlight", "PostgreSQL DSN")
+	//flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
+
 	flag.Parse()
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer db.Close()
+
 	app := &application{
 		config: cfg,
 		logger: logger,
@@ -38,6 +55,25 @@ func main() {
 		WriteTimeout: 30 * time.Second,
 	}
 	logger.Printf("Starting %s server on %s", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
 }
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("pgx", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+	//create context with timeout of 5 seconds
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	// Return the sql.DB connection pool.
+	return db, nil
+
+}
+
+//For russian fonts
+//psql \! chcp 1251
